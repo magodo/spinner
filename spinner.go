3f15501ceba763e86155fb02d53830d695d3fe96
magodo <wztdyl@sina.com>
2022-05-17 10:54:16 +0800

    Re-design API

--- spinner.go
+++ spinner.go
@@ -4,43 +4,63 @@ import (
 	"context"
 	"fmt"
 
-	bspinner "github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type Spinner struct {
-	ctx     context.Context
-	spinner bspinner.Model
-	msg     string
+func Run(f func(ctx context.Context), msg string, sp spinner.Model) error {
+	doneCh := make(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		f(ctx)
+		doneCh <- struct{}{}
+	}()
+	s := model{m: sp, msg: msg, doneCh: doneCh, cancel: cancel}
+	return tea.NewProgram(s).Start()
 }
 
-func New(ctx context.Context, msg string, spinner bspinner.Model) Spinner {
-	return Spinner{ctx: ctx, spinner: spinner, msg: msg}
+type model struct {
+	m   spinner.Model
+	msg string
+	// doneCh is used to receive the done msg from the invoked function.
+	doneCh <-chan struct{}
+	// cancel is used to cancel the ctx of the invoked function, to notify the invoked function that it is interrupted
+	cancel context.CancelFunc
+	// canceled indicates that the app is canceled
+	canceled bool
 }
 
-func (m Spinner) Init() tea.Cmd {
-	return m.spinner.Tick
+func (m model) Init() tea.Cmd {
+	return m.m.Tick
 }
 
-func (m Spinner) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	if m.ctx.Err() != nil {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	select {
+	case <-m.doneCh:
 		return m, tea.Quit
+	default:
 	}
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
-			return m, tea.Quit
+			m.cancel()
+			m.canceled = true
+			return m, nil
 		default:
 			return m, nil
 		}
 	default:
 		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
+		m.m, cmd = m.m.Update(msg)
 		return m, cmd
 	}
 }
 
-func (m Spinner) View() string {
-	return fmt.Sprintf("%s %s\n", m.spinner.View(), m.msg)
+func (m model) View() string {
+	if m.canceled {
+		return fmt.Sprintf("%s Stopping on interrupt\n", m.m.View())
+	}
+	return fmt.Sprintf("%s %s\n", m.m.View(), m.msg)
 }
--- spinner_test.go
+++ spinner_test.go
@@ -2,12 +2,9 @@ package spinner
 
 import (
 	"context"
-	"fmt"
-	"os"
 	"time"
 
 	"github.com/charmbracelet/bubbles/spinner"
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
@@ -15,10 +12,11 @@ func ExampleSpinner() {
 	s := spinner.New()
 	s.Spinner = spinner.Dot
 	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
-	p := tea.NewProgram(New(ctx, "Loading", s))
-	if err := p.Start(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	Run(func(ctx context.Context) {
+		time.Sleep(1 * time.Second)
+		if ctx.Err() != nil {
+			return
+		}
+		time.Sleep(3 * time.Second)
+	}, "sleeping", s)
 }
